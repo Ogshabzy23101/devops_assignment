fb618667ba7ac25f511946e5b89ea54825d472c0
damilola ogundiran <damilola@DESKTOP-MTM0E4M.localdomain>
2025-09-29 20:46:48 +0100

    task 1 complete

---
+++ task_A/task_a1/task_a.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+
+
+#####################
+#author: Damilola ogundiran
+#Date: 28th sept 2025
+#version: v2
+######################
+
+
+#create  a folder
+mkdir task_folder
+
+#change directory to the folder
+cd task_folder
+
+#create 3 files 
+touch file_1.txt file_2.txt file_3.txt
+echo "today is $(date) and i am $(whoami)"> file_3.txt
+
+#check if backup exist, else create new one
+backup="backup_file"
+if [ -d "$backup" ]; then
+	echo "backup directory already exist, copying...."
+       	cp file_3.txt "$backup";
+else
+	echo "creating backup directory and copying file..."
+	mkdir -p "$backup" && cp file_3.txt "$backup"
+fi
+
+#list files
+
+ls -ltr
+
+
