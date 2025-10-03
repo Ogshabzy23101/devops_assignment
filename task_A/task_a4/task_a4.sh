1bb823a01e96a3ca46f82577a740de5760a87db0
damilola ogundiran <damilola@DESKTOP-MTM0E4M.localdomain>
2025-10-03 22:28:47 +0100

    task 4 completed

--- task_A/task_a4/task_a4.sh
+++ task_A/task_a4/task_a4.sh
@@ -9,19 +9,49 @@
 
 set -euo pipefail
 
-##set a variable for the task folder
-#taskfolder="www"
-#
-###check if task folder already exist
-#if [ -d "$taskfolder" ]; then
-#        echo "overwriting task_folder";
-#                        rm -rf "$taskfolder"
-#                                else
-#                                                echo "creating task folder";
-#                                                fi
-#
-#                                                #create new taskfolder
-#                                                        mkdir -p "$taskfolder"
-#
-#                                                        #change directory to taskfolder
-#                                                                cd "$taskfolder"
+#create a branch for task a4
+	git checkout -b taska4
+
+#set a variable for the task folder
+	taskfolder="task_folder"
+
+#check if task folder already exist
+	if [ -d "$taskfolder" ]; then
+		echo "overwriting task_folder";
+		rm -rf "$taskfolder"
+	else
+		echo "creating task folder";
+	fi
+
+#create new taskfolder
+        mkdir -p "$taskfolder"
+
+#change directory to taskfolder
+	cd "$taskfolder"
+
+#create a sample file
+	echo "this is a sample file" > samplefile.txt
+
+#add changes to git then commit and push to origin
+	git add .
+	git commit -m "task folder and sample file created and added"
+	git push origin taska4
+
+#create a new branch for modifying sample file
+	git checkout -b taska4_feature
+
+#modify sample file
+	echo "sample file has now been modify" >> samplefile.txt
+
+#add to git then commit and modify
+	git add .
+	git commit -m "sample file modify"
+	git push origin taska4_feature
+
+#switch to taska4 branch and merge
+	git checkout taska4 && git merge taska4_feature
+
+	echo "script run succesfully"
+
+
+
