1b59a09aca9a35c642e63ee6b04a80275b4151eb
damilola ogundiran <damilola@DESKTOP-MTM0E4M.localdomain>
2025-10-03 22:07:21 +0100

    task 4 folder created and added

---
+++ task_A/task_a4/task_a4.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+#######################
+####author: Damilola ogundiran
+####Date: 28th sept 2025
+####version: v2
+#########################
+
+set -euo pipefail
+
+##set a variable for the task folder
+#taskfolder="www"
+#
+###check if task folder already exist
+#if [ -d "$taskfolder" ]; then
+#        echo "overwriting task_folder";
+#                        rm -rf "$taskfolder"
+#                                else
+#                                                echo "creating task folder";
+#                                                fi
+#
+#                                                #create new taskfolder
+#                                                        mkdir -p "$taskfolder"
+#
+#                                                        #change directory to taskfolder
+#                                                                cd "$taskfolder"
